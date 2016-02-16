1edf8fcc92c314219bfd2ada45e5ef6bd88811f5
Jessica Hathaway <hathaway.jess@gmail.com>
2016-02-15 17:57:29 -0700

    Finished

---
+++ homeCtrl.js
@@ -0,0 +1,16 @@
+app.controller('homeCtrl', function($scope) {
+	$scope.myProfile = 
+	{
+		// name: "Jessica",
+		friends: [{name: 'Ryan'}, {name: 'Bryan'}, {name: 'Sarah'}, {name: 'Zac'}, {name: 'Erin'}]
+	};
+	$scope.sortOptions = [{
+    display: 'Ascending'
+  , value: false
+  },
+  {
+    display: 'Descending'
+  , value: true
+  }
+];
+});
